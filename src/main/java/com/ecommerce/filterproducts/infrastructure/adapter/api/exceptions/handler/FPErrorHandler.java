81774eb82f810c136314cacf4eafab0edd6c7bb7
carlos <temvvo@gmail.com>
2023-05-05 07:36:54 -0300

    Error handler added and unit tests for ProductFinder added too

---
+++ src/main/java/com/ecommerce/filterproducts/infrastructure/adapter/api/exceptions/handler/FPErrorHandler.java
@@ -0,0 +1,30 @@
+package com.ecommerce.filterproducts.infrastructure.adapter.api.exceptions.handler;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.io.IOException;
+
+@Slf4j
+@ControllerAdvice
+public class FPErrorHandler {
+
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity defaultErrorHandler(HttpServletRequest req, Exception e) {
+
+        log.error("RestExceptionHandler Exception handler. Server error", e);
+
+        return new ResponseEntity<>("Server Error. ", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(value = IOException.class)
+    public ResponseEntity IOErrorHandler(HttpServletRequest req, Exception e) {
+
+        log.error("RestExceptionHandler Exception handler. File IO Error", e);
+
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
---
+++ src/test/java/com/ecommerce/filterproducts/application/ProductFinderTest.java
@@ -0,0 +1,45 @@
+package com.ecommerce.filterproducts.application;
+
+import com.ecommerce.filterproducts.domain.port.file.system.IProductFileSystem;
+import com.ecommerce.filterproducts.domain.port.file.system.ISizeFileSystem;
+import com.ecommerce.filterproducts.domain.port.file.system.IStockFileSystem;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ProductFinderTest {
+
+    @InjectMocks
+    private ProductFinder productFinder;
+    @Mock
+    private IProductFileSystem productFileSystem;
+
+    @Mock
+    private ISizeFileSystem sizeFileSystem;
+
+    @Mock
+    private IStockFileSystem stockFileSystem;
+
+
+    @Test
+    public void shouldGetAll() throws IOException {
+        productFinder.getAll();
+        verify(productFileSystem).getAll();
+    }
+
+    @Test
+    public void shouldGetAllVisibleIds() throws IOException {
+        productFinder.getProductsId();
+        verify(productFileSystem).getAll();
+        verify(sizeFileSystem).getAll();
+        verify(stockFileSystem).getAll();
+    }
+}
